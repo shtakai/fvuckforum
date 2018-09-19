69fa646eddfc6e2b275e07d9b8aa333bccf4af4c
Alyson Takai <shtakai@users.noreply.github.com>
2018-09-19 20:54:44 +0700

    Handle NotFound Page (#8)
    * Add PageNotFound component
    
    * Add default route

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="col-full">
+    <h1>Not Found</h1>
+    <p>Ooops, what are you fucking for something fucked up morons?
+      <router-link :to="{name: 'Home'}">
+        Kill em all!
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 100px;
+}
+
+p {
+  font-size: 50px;
+  font-weight: 100;
+}
+
+div {
+  text-align: center;
+}
+</style>
