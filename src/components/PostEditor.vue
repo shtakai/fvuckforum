c5ff65c44d65b9e1a7d669b2816ac2cb75a60aea
Alyson Takai <shtakai@gmail.com>
2018-09-20 21:59:58 +0700

    Create PostEditor component

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,51 @@
+<template>
+  <form @submit.prevent="save">
+    <div class="form-group">
+      <textarea
+        class="form-input"
+        v-model="text"
+      ></textarea>
+    </div>
+    <div class="form-actions">
+      <button class="btn-blue">Submit post</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    threadId: {
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      const postId = 'greatPost' + Math.random()
+      const post = {
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.threadId,
+        userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2',
+        '.key': postId
+      }
+
+      this.text = ''
+
+      this.$emit(
+        'save', {post}
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
