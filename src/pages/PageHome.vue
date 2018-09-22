07ce68715b4e1160d55b076293f89ffc526c3481
Alyson Takai <shtakai@users.noreply.github.com>
2018-09-22 15:17:40 +0700

    Feature/ Organise thread (#17)
    * create PageForum component
    
    * Add route for PageForum component
    
    * Add prettifier
    
    * Add ForumList component
    
    * Add ForumListItem component
    
    * Modify to use ForumList on PageHome
    
    * Add prettier
    
    * Add category components
    
    * Modify to use category components

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="col-full">
     <h1>Fucked up vuckorum.</h1>
-    <ThreadList :threads="threads"/>
+    <CategoryList :categories="categories"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import ThreadList from '@/components/ThreadList'
+import CategoryList from '@/components/CategoryList'
 
 export default {
   components: {
-    ThreadList
+    CategoryList
   },
 
   data () {
     return {
-      threads: Object.values(sourceData.threads),
-      posts: sourceData.posts,
-      users: sourceData.users
+      categories: Object.values(sourceData.categories)
     }
   }
 }
