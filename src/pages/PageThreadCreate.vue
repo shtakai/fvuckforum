f792b8ba8c9e7f4ea7ebe652b66a041308cfdc24
Alyson Takai <shtakai@gmail.com>
2018-09-23 20:56:23 +0700

    Add implementation of save()

--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -52,7 +52,14 @@ export default {
 
   methods: {
     save () {
-      // dispatch action
+      this.$store.dispatch(
+        'createThread',
+        {
+          forumId: this.forum['.key'],
+          title: this.title,
+          text: this.text
+        }
+      )
     }
   }
 }
