76545cd7754ad9370e9a99c1cd62fd742163bf9a
Alyson Takai <shtakai@gmail.com>
2018-09-23 17:27:54 +0700

    Add PageThreadCreate component

---
+++ src/pages/PageThreadCreate.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="col-full push-top">
+    <h1>Create a fucked up thread in <i>{{forum.name}}</i></h1>
+
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="thread_title">Title:</label>
+        <input
+          v-model="title"
+          type="text"
+          name="title"
+          id="thread_title"
+          class="form-input"
+        >
+      </div>
+
+      <div class="form-grou">
+        <label for="thread_title">Content:</label>
+        <textarea
+          v-model="text"
+          name="content"
+          id="thread_content"
+          row="8"
+          cols="148"
+          class="form-input"
+        >
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    forum: {
+      required: true,
+      type: Object
+    }
+  },
+
+  data () {
+    return {
+      title: '',
+      text: ''
+    }
+  },
+
+  methods: {
+    save () {
+      // dispatch action
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
