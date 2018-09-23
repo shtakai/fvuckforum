be190bccacc9e90f620ad68b1bd05b3d7a9833a6
Alyson Takai <shtakai@users.noreply.github.com>
2018-09-23 16:43:18 +0700

    Feature/shared component (#23)
    * Add ProfileEdit route
    
    * Modify active both components with edit condition
    
    * Modify to use router-link
    
    * Add route.push
    
    * Modify cancel button to use action

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex-grid">
-    <!-- <UserProfileCard
+    <UserProfileCard
+      v-if="!edit"
       :user="user"
       :userPostsCount="userPostsCount"
       :userThreadsCount="userThreadsCount"
-    /> -->
+    />
     <UserProfileCardEditor
+      v-else
       :user="user"
       :userPostsCount="userPostsCount"
       :userThreadsCount="userThreadsCount"
@@ -38,6 +40,13 @@ export default {
     PostList,
     UserProfileCard,
     UserProfileCardEditor
+  },
+
+  props: {
+    edit: {
+      type: Boolean,
+      default: false
+    }
   },
 
   computed: {
