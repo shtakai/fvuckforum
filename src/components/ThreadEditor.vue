943e6fe43841b7b32d2d614d65e7c415c542d97a
Alyson Takai <shtakai@gmail.com>
2018-09-24 21:19:13 +0700

    Refactor to improve ThreadEditor

--- src/components/ThreadEditor.vue
+++ src/components/ThreadEditor.vue
@@ -25,7 +25,7 @@
 
     <div class="btn-group">
       <button @click.prevent="cancel" class="btn btn-ghost">Cancel</button>
-      <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
+      <button class="btn btn-blue" type="submit" name="Publish">{{isUpdate ? 'Update' : 'Publish'}}</button>
     </div>
   </form>
 </template>
@@ -49,6 +49,12 @@ export default {
         title: this.title,
         text: this.text
       }
+    }
+  },
+
+  computed: {
+    isUpdate () {
+      return !!this.title
     }
   },
 
