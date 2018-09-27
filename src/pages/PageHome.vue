3c85b4f3ff2f2feb688dee611167ada8f95e34c6
Alyson Takai <shtakai@gmail.com>
2018-09-27 21:42:13 +0700

    [WIP]Add life cycle
    PR is not merged

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -18,6 +18,26 @@ export default {
     return {
       categories: Object.values(this.$store.state.categories)
     }
+  },
+
+  beforeCreate () {
+    console.log('BEFORE CREATE', this.categories)
+  },
+  created () {
+    console.log('CREATED', this.categories)
+  },
+  beforeMount () {
+    console.log('BEFORE MOUNT', this.categories)
+  },
+  mounted () {
+    console.log('MOUNTED', this.categories, this.$el.innerText.length)
+  },
+  beforeDestroy () {
+    console.log('BEFORE DESTROY: turn off listeners', this.categories)
+  },
+  destroyed () {
+    console.log('DESTROYED', this.categories)
   }
+
 }
 </script>
