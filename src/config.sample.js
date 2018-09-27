f2236422ed3faf40520c909d38d6cf9b0a9b8f68
Alyson Takai <shtakai@gmail.com>
2018-09-27 21:22:07 +0700

    Add sample configuration file

---
+++ src/config.sample.js
@@ -0,0 +1,12 @@
+// provided from Firebas
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+}
+
+export {config}
