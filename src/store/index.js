f4c7af41684f99df24f6d425611cfbb839846c20
Alyson Takai <shtakai@gmail.com>
2018-09-27 21:01:40 +0700

    Fix userThreadsCount
    From `.posts` to `.threads`

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,7 @@ export default new Vuex.Store({
       return state.users[state.authId]
     },
     userPostsCount: state => id => countObjectProperties(state.users[id].posts),
-    userThreadsCount: state => id => countObjectProperties(state.users[id].posts),
+    userThreadsCount: state => id => countObjectProperties(state.users[id].threads),
     threadRepliesCount: state => id => countObjectProperties(state.threads[id].posts) - 1
   },
 
