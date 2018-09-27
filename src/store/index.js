cfe832cdca8bbfe72b82da6c5271e5048014b80a
Alyson Takai <shtakai@gmail.com>
2018-09-27 20:23:56 +0700

    Add mutations w/high orders function

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,15 @@ import {countObjectProperties} from '@/helpers/utils'
 
 Vue.use(Vuex)
 
+const makeAppendChildToParentMutation = ({parent, child}) =>
+  (state, {childId, parentId}) => {
+    const resource = state[parent][parentId]
+    if (!resource[child]) {
+      Vue.set(resource, child, {})
+    }
+    Vue.set(resource[child], childId, childId)
+  }
+
 export default new Vuex.Store({
   state: {
     ...sourceData,
