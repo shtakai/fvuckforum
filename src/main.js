5ca685f66e6821f5e4fb2150fa1ffe80f6a76bac
Alyson Takai <shtakai@gmail.com>
2018-10-04 20:29:49 +0700

    Modify fetching user beforeCreate App

--- src/main.js
+++ src/main.js
@@ -19,5 +19,8 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate () {
+    store.dispatch('fetchUser', {id: store.state.authId})
+  }
 })
